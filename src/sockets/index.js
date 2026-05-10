819e73233afd97aa3741ffefcd0639a7d601ec66
Shriyansh Ozarkar <sozarkar7@gmail.com>
2026-05-10 17:53:04 +0530

    fix: update CORS configuration to use CLIENT_URL for security

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,9 +1,12 @@
 import { Server } from "socket.io";
 
 export const initSocket = (server) => {
-  const io = new Server(server, {
-    cors: { origin: "*" },
-  });
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST"]
+  }
+});
 
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
